f48baf3829128ea78c18e1aa726e870e2cd79ebf
Sandith Pivithuru <sandithpivithuru@gmail.com>
2025-01-07 14:36:07 +0530

    Configure Employee Model

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Employee extends Model
 {
-    //
+    protected $table = 'employees';
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'name',
+        'email',
+        'address',
+        'phone',
+    ];
+    use HasFactory;
 }
